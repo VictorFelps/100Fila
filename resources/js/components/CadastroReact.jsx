8aa12f8109c4e319d03d42bbd8f8f6e1a9c36c14
tr26202es <victor.vieira@trf1.jus.br>
2023-10-30 16:50:24 -0300

    tela cadastro

--- resources/js/components/CadastroReact.jsx
+++ resources/js/components/CadastroReact.jsx
@@ -9,36 +9,42 @@ import Card from 'react-bootstrap/Card';
 export default function CadastroReact() {
     return (
         <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
-            <Card style={{ width: '400px', height: '400px' }}>
-                <Card.Body>
-                    <Card.Title>Formulário de Login</Card.Title>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
+    <Card style={{ width: '400px', height: '400px' }}>
+        <Card.Body>
+            <Card.Title>Formulário de Cadastro</Card.Title>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Nome</Form.Label>
+                    <Form.Control type="text" placeholder="Digite seu nome" />
+                </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Endereço de E-mail</Form.Label>
+                    <Form.Control type="email" placeholder="Digite seu email" />
+                    <Form.Text className="text-muted">
+                        Nunca compartilharemos seu e-mail com mais ninguém.
+                    </Form.Text>
+                </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Senha</Form.Label>
+                    <Form.Control type="password" placeholder="Digite sua senha" />
+                </Form.Group>
 
-                        <div className="d-grid gap-2">
-                            <Button variant="primary" type="submit">
-                                Submit
-                            </Button>
-                        </div>
-                    </Form>
-                </Card.Body>
-            </Card>
-        </Container>
+                <Form.Group className="mb-3" controlId="formBasicPasswordConfirmation">
+                    <Form.Label>Confirme sua senha</Form.Label>
+                    <Form.Control type="password" placeholder="Confirme sua senha" />
+                </Form.Group>
+
+                <div className="d-grid gap-2">
+                    <Button variant="primary" type="submit">
+                        Cadastrar
+                    </Button>
+                </div>
+            </Form>
+        </Card.Body>
+    </Card>
+</Container>
     );
 }
 
