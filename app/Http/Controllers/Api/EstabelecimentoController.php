2088402d2633862c92e31cd54d1c8d89bc88c244
tr26202es <victor.vieira@trf1.jus.br>
2023-10-31 16:16:24 -0300

    adicionando resource

---
+++ app/Http/Controllers/Api/EstabelecimentoController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Estabelecimento;
+use Illuminate\Http\Request;
+
+class EstabelecimentoController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Estabelecimento::all());
+    }
+
+    public function show($id)
+    {
+        $estabelecimento = Estabelecimento::find($id);
+
+        if(!$estabelecimento) {
+            return response()->json([
+                'message' => 'Estabelecimento não encontrado'
+            ], 500);
+        }
+
+        return response()->json(Estabelecimento::find($id));
+    }
+
+    public function edit(Request $request, string $id)
+    {
+        $estabelecimento = Estabelecimento::find($id);
+
+        if(!$estabelecimento) {
+            return response()->json([
+                'message' => 'Estabelecimento não encontrado'
+            ], 404);
+        }
+        
+        $estabelecimento->update($request->all());
+
+        return response()->json(Estabelecimento::find($id), 204);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\EstabelecimentoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -27,3 +28,5 @@ Route::get('usuarios', function() {
         
     ]);
 });
+
+Route::resource('estabelecimento', EstabelecimentoController::class);
