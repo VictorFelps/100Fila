135013dec791a91cbb09e9ec9b48d471495aa82f
tr26202es <victor.vieira@trf1.jus.br>
2023-10-31 15:32:44 -0300

    adicionando env e sqlite

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeReact from './HomeReact'; // Importe o componente HomeReact
 import CadastroReact from './CadastroReact'; // Importe o componente CadastroReact
+import LoginReact from './LoginReact';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<HomeReact />} />
         <Route path="/cadastro" element={<CadastroReact />} />
+        <Route path="/login" element={<LoginReact />} />
       </Routes>
     </Router>
   );
