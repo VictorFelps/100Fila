43b1359eaa5257710b894361404f228bf5dc2c65
tr26202es <victor.vieira@trf1.jus.br>
2023-11-03 14:49:31 -0300

    Cadastro Funcional

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\EstabelecimentoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,23 +9,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('usuarios', function() {
-    sleep(2);
-    return response()->json([
-        ['id' => 1, 'nome' => 'estabelecimento 1'],
-        ['id' => 2, 'nome' => 'estabelecimento 2'],
-        ['id' => 3, 'nome' => 'estabelecimento 3'],
-        
-    ]);
-});
-
-Route::resource('estabelecimento', EstabelecimentoController::class);
