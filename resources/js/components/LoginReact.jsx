3a7e95d0734d7c8fe8bb451b4c52a67c11708841
Victor <victorfsv@gmail.com>
2023-11-29 16:10:36 -0300

    Validação da senha

--- resources/js/components/LoginReact.jsx
+++ resources/js/components/LoginReact.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -7,20 +8,19 @@ import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router';
 import backgroundImage from './imagem.jpg';
 
-
 export default function LoginReact() {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [loginError, setLoginError] = useState(false);
+
     const {
         register,
         handleSubmit,
-        watch,
         formState: { errors },
-    } = useForm()
+    } = useForm();
 
     const onSubmit = (data) => {
         const _token = document.querySelector('[name="csrf-token"]').getAttribute("content");
 
-        alert(JSON.stringify(data));
         fetch('/login', {
             method: 'POST', 
             headers: {
@@ -29,38 +29,45 @@ export default function LoginReact() {
                 'Accept': 'application/json'
             },
             body: JSON.stringify({...data, _token})
-        }).then(result => {
-            console.log(result)
-            navigate('/')
         })
-        .catch(error => console.log('erro', error))
+        .then(response => {
+            if (response.ok) {
+                // Login bem-sucedido, redirecione ou faça o que for necessário
+                navigate('/');
+            } else {
+                // Login falhou, exibir mensagem de erro
+                setLoginError(true);
+            }
+        })
+        .catch(error => console.log('erro', error));
     };
 
     return (
         <div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}>
-        <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
-            <Card style={{ width: '400px', height: '300px' }}>
-                <Card.Body>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Endereço de E-mail</Form.Label>
-                            <Form.Control type="email" placeholder="Digite seu email" {...register("email", { required: true })} />
-                        </Form.Group>
+            <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
+                <Card style={{ width: '400px', height: '300px' }}>
+                    <Card.Body>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Endereço de E-mail</Form.Label>
+                                <Form.Control type="email" placeholder="Digite seu email" {...register("email", { required: true })} />
+                            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Senha</Form.Label>
-                            <Form.Control type="password" placeholder="Digite sua senha" {...register("password", { required: true })} />
-                        </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Senha</Form.Label>
+                                <Form.Control type="password" placeholder="Digite sua senha" {...register("password", { required: true })} />
+                                {loginError && <p className="text-danger">Credenciais inválidas. Por favor, tente novamente.</p>}
+                            </Form.Group>
 
-                        <div className="d-grid gap-2">
-                            <Button variant="primary" type="submit" onClick={handleSubmit(onSubmit)}>
-                                Login
-                            </Button>
-                        </div>
-                    </Form>
-                </Card.Body>
-            </Card>
-        </Container>
+                            <div className="d-grid gap-2">
+                                <Button variant="primary" type="submit" onClick={handleSubmit(onSubmit)}>
+                                    Login
+                                </Button>
+                            </div>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </Container>
         </div>
     );
 }
